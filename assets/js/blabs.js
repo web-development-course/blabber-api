c191b482b80b7a5e3e8b535347d1ee5abd459b6c
Guy Sopher <guyso@wix.com>
2017-10-01 19:36:37 +0300

    support api requests

--- assets/js/blabs.js
+++ assets/js/blabs.js
@@ -1,47 +1,77 @@
 function createBlab(blabData) {
-  var newBlab = $(`
-    <div id="${blabData.id}" class="blab-container">
-      <div class="blab-image">
-        <img src="https://email2pic.herokuapp.com/gravatar/${blabData.email}">
-      </div>
-      <div class="blab-body">
-        <div class="blab-email">${blabData.email}</div>
-        <div class="blab-content">${blabData.content}</div>
-        <div class="blab-actions">
-          <a href="#" class="delete-blab" onclick="deleteBlab('${blabData.id}')">delete</a>
-          <span> · </span>
-          <a href="#" class="upvote-blab" onclick="upvoteBlab('${blabData.id}')">upvote (<span class="blab-upvotes">${blabData.upvotes}</span>)</a>
-          <span> · </span>
-          <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=${encodeURI(document.location.host)}%2Fblabs%2F${blabData.id}&t=${blabData.content}">share</a>
-          <span> · </span>
-          <a>${blabData.createdAt.toLocaleString()}</a>
-        </div>
-      </div>
-    </div>
-  `);  
+  var newBlab = $('' + 
+    '<div id="' + blabData.id + '" class="blab-container">' +
+      '<div class="blab-image">' +
+        '<img src="https://email2pic.herokuapp.com/gravatar/' + blabData.email + '">' +
+      '</div>' +
+      '<div class="blab-body">' +
+        '<div class="blab-email">' + blabData.email + '</div>' +
+        '<div class="blab-content">' + blabData.content + '</div>' +
+        '<div class="blab-actions">' +
+          '<a href="#" class="delete-blab" onclick="deleteBlab(\'' + blabData.id + '\')">delete</a>' +
+          '<span> · </span>' +
+          '<a href="#" class="upvote-blab" onclick="upvoteBlab(\'' + blabData.id + '\')">upvote (<span class="blab-upvotes">' + blabData.upvotes + '</span>)</a>' +
+          '<span> · </span>' +
+          '<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(document.location.host) + '%2Fblab%2F' + blabData.id + '&t=' + blabData.content + '">share</a>' +
+          '<span> · </span>' +
+          '<a>' + blabData.createdAt.toLocaleString() + '</a>' +
+        '</div>' +
+      '</div>' +
+    '</div>' +
+  '');  
   $('#blabs-container').prepend(newBlab);
 }
 
 function deleteBlab(blabId) {
-  $('#' + blabId).remove();
+  debugger;
+  $.ajax({
+    url: '/blab/destroy/' + blabId,
+    type: 'delete',
+    data: $(this).serialize(),
+    success: function () {
+      $('#' + blabId).remove();
+    }
+  });
 }
 
 function upvoteBlab(blabId) {
+  debugger;
   var upvotes = $('#' + blabId).find('.blab-upvotes');
-  upvotes.html(parseInt(upvotes.html()) + 1);
+  var upvotesNum = parseInt(upvotes.html());
+  $.ajax({
+    url: '/blab/update/' + blabId + '?upvotes=' + (upvotesNum + 1),
+    type: 'put',
+    data: $(this).serialize(),
+    success: function (res) {
+      upvotes.html(res.upvotes);
+    }
+  });
 }
 
 $(document).ready(function () {
 
+  $.ajax({
+    url: '/blab',
+    type: 'get',
+    success: function (blabs) { 
+      for (var i = 0; i < blabs.length; i++) {
+        createBlab(blabs[i]);
+      }
+    }
+  });
+
+
   $('#create-blab').submit(function (e) {
     e.preventDefault();
-    var data = {
-      id: `blab_${Math.floor(Math.random() * 1000000)}`,
-      email: $(this.email).val(),
-      content: $(this.content).val(),
-      upvotes: 1,
-      createdAt: new Date(),
-    };
-    createBlab(data);
+
+    $.ajax({
+      url: '/blab/create',
+      type: 'post',
+      data: $(this).serialize(),
+      success: function (data) {
+        createBlab(data);
+      }
+    });
+
   });
 });
